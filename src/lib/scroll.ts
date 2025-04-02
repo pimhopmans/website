b04b70cd5f004c1cb580a454b0fca64e8d4b23e2
Pim Hopmans <pim.hopmans@live.nl>
2025-04-02 18:18:22 +0200

    chore: fix ts warning

--- src/lib/scroll.ts
+++ src/lib/scroll.ts
@@ -1,6 +1,5 @@
 export function smoothScroll(id: string) {
-  // @ts-expect-error don't know how to fix
-  document?.querySelector(id).scrollIntoView({
+  document?.querySelector(id)?.scrollIntoView({
     behavior: "smooth",
   });
 }
